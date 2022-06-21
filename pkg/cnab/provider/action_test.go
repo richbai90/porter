a4cdb1c89cdc3d340d38f68289227e6794c2bf34
Rich Baird <Rich@rbaird.me>
2022-06-21 18:24:03 -0500

    Add a test to test template strings in extension configuration

--- pkg/cnab/provider/action_test.go
+++ pkg/cnab/provider/action_test.go
@@ -1,10 +1,14 @@
 package cnabprovider
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"testing"
 
+	"get.porter.sh/porter/pkg/cnab"
+	"get.porter.sh/porter/pkg/config"
+	"get.porter.sh/porter/pkg/storage"
 	"github.com/cnabio/cnab-go/bundle"
 	"github.com/cnabio/cnab-go/driver"
 	"github.com/stretchr/testify/assert"
@@ -40,4 +44,49 @@ func TestAddRelocation(t *testing.T) {
 	assert.Equal(t, string(data), mapping)
 	assert.Equal(t, "my.registry/microservice@sha256:cca460afa270d4c527981ef9ca4989346c56cf9b20217dcea37df1ece8120687", op.Image.Image)
 
+}
+
+func TestRuntime_Execute(t *testing.T) {
+	type args struct {
+		ctx  context.Context
+		args ActionArguments
+		cfg  *config.Config
+	}
+
+	r := NewTestRuntime(t)
+	b, _ := ioutil.ReadFile("testdata/template-bundle.json")
+	bun := bundle.Bundle{}
+	json.Unmarshal(b, &bun)
+	xbun := cnab.NewBundle(bun)
+
+
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Test Execute With Bundle Parameters",
+			args: args{
+				ctx: context.TODO(),
+				args: ActionArguments{
+					Action: "install",
+					Installation: r.TestInstallations.CreateInstallation(storage.Installation{}),
+					BundleReference: cnab.BundleReference{Reference: cnab.OCIReference{}, Definition: xbun},
+					Driver: "docker",
+					AllowDockerHostAccess: true,
+					Params: make(map[string]interface{}),
+					},
+				cfg: r.Config,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := r.Execute(tt.args.ctx, tt.args.args, tt.args.cfg); (err != nil) != tt.wantErr {
+				t.Errorf("Runtime.Execute() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
